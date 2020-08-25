a927b2019c1bdb6603bccb944cc16ec306b0b91f
guimroque <62621598+guimroque@users.noreply.github.com>
2020-08-24 22:32:02 -0300

    v1.0

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const session = require("express-session");
-
+const path = require('path')
+const fileUpload = require('express-fileupload')
+var generateSafeId = require('generate-safe-id');
 
 // View engine
 app.set('view engine','ejs');
+app.set('views', __dirname + '/views'); //Local onde vai ser salvo o file
+app.use(express.static(path.join(__dirname, 'public')));//local dos arquivos estaticos
 
 app.use(session({
     secret: "qualquercoisa", cookie: { maxAge: 30000000},
@@ -13,11 +17,11 @@ app.use(session({
     resave: true
 }))
 
-app.use(express.static('public'));
 
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(fileUpload());//setando uso da biblioteca fileupload
 
 
 // Router
@@ -25,6 +29,19 @@ app.use(bodyParser.json());
     app.get("/", (req, res) => {
         res.render("index.ejs");
     })
+    app.post('/', (req, res)=>{
+
+        console.log(req.body)
+        var file = req.files.uploaded_image;
+        var id = generateSafeId();
+        var img_name=id+file.name;
+        console.log(img_name)
+        file.mv('public/images/upload_images/'+img_name, function(err) {
+        //envia para o banco de dados, apenas salvamos imagem com id gerado e usamos apenas o endereço para acessamor do BD.
+            if (err)
+              return res.status(500).send(err);
+                 });
+    });
 
 // End Router
 app.listen(3000, () => {
